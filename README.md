90d960c9ee20a0b6f25e97b9c5d6046aa6e0f65b
byoo-myoo <deb.myojin@gmail.com>
2025-02-24 02:43:38 +0900

    A02まで解いた

--- README.md
+++
@@ -1,7 +0,0 @@
-# atcoder_study
-
-以下を参考に環境構築
-
-[https://qiita.com/cosocaf/items/255003ecec1d3badfc7b](https://qiita.com/cosocaf/items/255003ecec1d3badfc7b)
-
----
---
+++ 演習問題/A02.ts
@@ -0,0 +1,17 @@
+
+export const A02 = () => {
+  const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n");
+
+// 1行目: N, X を取得
+const [N, X] = input[0].split(" ").map(Number);
+
+// 2行目の A1, A2, ..., AN を取得
+const A = input[1].split(" ").map(Number);
+
+// X が A の中にあるかを判定
+console.log(A.includes(X) ? "Yes" : "No");
+
+}
